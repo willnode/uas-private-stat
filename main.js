580d8169ed9a99355ebe57b532a6cea4ada7c8fc
Wildan Mubarok <willnode@wellosoft.net>
2018-02-12 06:46:36 +0700

    🍻 better style + logic
    + toggleable + two column script section
    + property choices (and dollar regex fix)
    + JSON request now uses textarea instead prompt (truncated by browser?!)
    + error catch in JSON processing
    + fallback default month by -1 (as current month is incomplete)
    + change color as by name (much more difference)
    + trim JS

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 var date = (new Date().getFullYear() - 2010) * 12 + new Date().getMonth();
 var canvas = document.getElementById('graph');
-var usdfilter = /\$ ([\d\.])$/;
+var usdfilter = /\$ ([\d\.-]+)/;
 var data = {
-	start: monthSerial(date - 11),
-	end: monthSerial(date),
+	start: monthSerial(date - 12),
+	end: monthSerial(date - 1),
 	startRaw: () => unmonthSerial(data.start),
-	endRaw: () => unmonthSerial(data.end),
+	endRaw: () => Math.max(data.startRaw(), unmonthSerial(data.end)),
 	dateRange: Array(date + 1).fill().map((_, i) => monthSerial(i)).reverse(),
 	pubId: localStorage['uas-priv-pubid'] || 0,
 	pubIdHint: 'https://i.imgur.com/uBFYQMA.png',
 	operation: 'downloads',
 	operationChoices: ['downloads', 'sales'],
+	property: 'qty',
+	propertyChoices: ['qty', 'gross'],
 	range: [],
 	raw: [],
 	parsed: [],
 	idx: 0,
+	showCodePanel: false,
 	theJS: '',
-	theData: localStorage['uas-priv-data'] || '',
+	theData: '',
+	theDataError: '',
+	loadTheDataFromDisk: function () {
+		data.theData = localStorage['uas-priv-data-' + data.operation];
+		setTimeout(() => data.goStep2(), 0);
+	},
 	// Generate the JS
 	goStep1: () => {
 		var range = new Array(data.endRaw() - data.startRaw() + 1).fill().map((_, i) => monthSerial(i + data.startRaw()));
@@ -24,52 +32,60 @@ var data = {
 	},
 	// Parse the data
 	goStep2: () => {
-		var sales = data.operation === 'sales';
-		localStorage['uas-priv-pubid'] = data.pubId;
-		localStorage['uas-priv-dates'] = JSON.stringify({ start: data.start, end: data.end });
-		data.raw = JSON.parse(localStorage['uas-priv-data'] = data.theData);
-		var parsed = [];
-		var iter = 0;
-		data.raw.forEach(el => {
-			if (typeof el === 'object') {
-				var aaData = el.aaData;
-				var result = el.result;
-				for (let i = 0; i < aaData.length; i++) {
-					var name = aaData[i][0];
-					var qty = parseFloat(sales ? aaData[i][2] : aaData[i][1]);
-					var gross = sales ? parseFloat(aaData[i][1].match(usdfilter)[0]) : 0;
-					var net = sales ? parseFloat(aaData[i][5].match(usdfilter)[0]) : 0;
-					var short = result[i]['short_url'];
-					var obj = parsed.find((v, i, a) => v.short === short);
-					if (!obj) parsed.push(obj = { name: name, short: short, stat: new Array(data.raw.length).fill(), total: { qty: 0, gross: 0, net: 0 } });
-					obj.stat[iter] = {
-						qty: qty,
-						gross: gross,
-						net: net,
-					};
-					obj.total.qty += qty;
-					obj.total.gross += gross;
-					obj.total.net += net;
+		try {
+			var sales = data.operation === 'sales';
+			localStorage['uas-priv-pubid'] = data.pubId;
+			localStorage['uas-priv-dates'] = JSON.stringify({ start: data.start, end: data.end });
+			data.raw = JSON.parse(localStorage['uas-priv-data-' + data.operation] = data.theData);
+			var parsed = [];
+			var iter = 0;
+			data.raw.forEach(el => {
+				if (typeof el === 'object') {
+					var aaData = el.aaData;
+					var result = el.result;
+					var snapst = el.snapshot;
+					for (let i = 0; i < aaData.length; i++) {
+						var name = aaData[i][0];
+						var qty = parseFloat(sales ? aaData[i][2] : aaData[i][1]);
+						var gross = sales ? parseFloat(aaData[i][5].match(usdfilter)[1]) : 0;
+						var short = result[i]['short_url'];
+						var obj = parsed.find((v, i, a) => v.short === short);
+						if (!obj) parsed.push(obj = { name: name, short: short, stat: new Array(data.raw.length).fill(), total: { qty: 0, gross: 0 } });
+						var st = {
+							qty: qty,
+							gross: gross,
+						};
+						obj.stat[iter] = combineStat(obj.stat[iter], st);
+						obj.stat[iter].snapshot = snapst;
+						obj.total = combineStat(obj.total, st);
+					}
 				}
-			}
-			iter++;
-		});
-
-		data.parsed = parsed;
+				iter++;
+			});
+			data.theDataError = '';
+			data.parsed = parsed;
+			data.goStep3();
+		} catch (e) {
+			console.log(e);
+			data.theDataError = e && e.message;
+		}
 	},
 	// Graph the whole thing
 	goStep3: () => {
 		var buffer = [];
 
 		data.parsed.forEach(el => {
+			var stat2 = [];
+			data.range.forEach(r => stat2.push(el.stat.find(v => v && v.snapshot === r) || null));
+
 			buffer.push({
 				label: el.name,
 				backgroundColor: 'transparent',
-				borderColor: serialColor(el.short),
-				data: el.stat.map((v, i) => (v && v.qty) || 0),
+				borderColor: serialColor(el.name),
+				data: stat2.map(v => (v && v[data.property]) || 0),
 			})
 		})
-		console.log(buffer.length);
+		if (window.chart) window.chart.destroy();
 		window.chart = new Chart(canvas.getContext("2d"), {
 			type: 'line',
 			data: {
@@ -77,7 +93,15 @@ var data = {
 				datasets: buffer
 			},
 		})
-
+	},
+	copyTheJS: function () {
+		var area = document.getElementById('theJS');
+		area.select();
+		area.focus();
+		var ok = document.execCommand('copy');
+		var btn = document.getElementById('copyTheJS-btn');
+		btn.textContent = ok ? 'Copied!' : 'Press Ctrl + C';
+		setTimeout(() => btn.textContent = 'Copy', 3000);
 	}
 }
 
@@ -92,12 +116,9 @@ new Vue({
 			data.end = dates.end;
 			if (data.pubId > 0) {
 				data.goStep1();
-				if (data.theData) {
-					data.goStep2();
-					data.goStep3();
-				}
 			}
 		}
+		data.loadTheDataFromDisk();
 	}
 })
 
@@ -107,23 +128,32 @@ function makeTheJS(operation, pubid, requests) {
 	var uri = '/api/publisher-info/${operation}/${pubid}/';
 	var data = [];
 	var requests = ${JSON.stringify(requests)};
-	var finalmsg = "Requests done. Please copy this data and proceed to next step.";
+	var finalmsg = "Requests done. Scroll down the page and hit Ctrl + C to copy the data";
 
 	function DoRequest(i) {
-		console.log("Requesting data at " + requests[i]);
+		var req = requests[i];
+		console.log("Requesting data at " + req);
 		var xhr = new XMLHttpRequest();
-		var url = uri + requests[i] + ".json";
+		var url = uri + req + ".json";
 		xhr.onreadystatechange = function () {
 			if (this.readyState == 4) {
 				if (this.status == 200)
-					data.push(JSON.parse(this.responseText));
+					data.push(Object.assign(JSON.parse(this.responseText), {snapshot:req}));
 				else
 					data.push("⛔ " + this.status);
 
 				if (i + 1 < requests.length)
 					DoRequest(i + 1);
-				else
-					prompt(finalmsg, JSON.stringify(data));
+				else {
+					var area = document.createElement("textarea");
+					document.body.appendChild(area);
+					area.value = JSON.stringify(data);
+					area.style.width = '100%';
+					area.style.height = '20%';
+					area.select();
+					area.focus();
+					console.log(finalmsg);
+				}
 			}
 		};
 		xhr.open("GET", url, true);
@@ -134,7 +164,7 @@ function makeTheJS(operation, pubid, requests) {
 	setTimeout(() => DoRequest(0), 0);
 	return "Wait a minute while we doing " + requests.length + " requests";
 })();
-`.replace(/\t/g, " ");
+`.trim().replace(/\t/g, " ");
 }
 
 function monthSerial(number) {
@@ -159,4 +189,13 @@ function serialColor(str) {
 		colour += ('00' + value.toString(16)).substr(-2);
 	}
 	return colour;
+}
+
+function combineStat(a, b) {
+	if (!a) return b;
+	if (!b) return a;
+	return {
+		qty: a.qty + b.qty,
+		gross: a.gross + b.gross,
+	}
 }
